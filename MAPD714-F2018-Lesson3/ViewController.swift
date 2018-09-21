e831cd4e8023d5fcdc3ab32fd45c2712876ee4e7
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-09-20 22:34:07 -0400

    added button images and number code

--- MAPD714-F2018-Lesson3/ViewController.swift
+++ MAPD714-F2018-Lesson3/ViewController.swift
@@ -1,25 +1,50 @@
-//
-//  ViewController.swift
-//  MAPD714-F2018-Lesson3
-//
-//  Created by Tom Tsiliopoulos on 2018-09-20.
-//  Copyright © 2018 CentennialCollege. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Instance Variables
+    var hasDecimal:Bool = false
+    
+    
+    // IBOutlets
+    @IBOutlet weak var resultArea: UITextField!
+    
+    
+    // lifecycle functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
+    // IBActions
+
+    @IBAction func onNumberButtonPress(_ sender: UIButton) {
+        
+        if(resultArea.text == "0") {
+            
+            if(sender.tag != -1) {
+                resultArea.text = ""
+                
+            }
+            else {
+                if(hasDecimal) {
+                    resultArea.text = "0"
+                }
+            }
+            
+        }
+        
+        if(sender.tag == -1) {
+            if(!hasDecimal) {
+                resultArea.text?.append(".")
+                hasDecimal = true
+            }
+        }
+        else {
+          resultArea.text?.append(String(sender.tag))
+        }
+        
+    }
+    
 }
 
